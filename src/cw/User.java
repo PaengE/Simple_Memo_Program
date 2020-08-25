9433018811fa42be567c05e23623903fbc59f300
cw <ckddn2820@gmail.com>
2020-08-25 18:36:19 +0900

    user-object class

--- src/cw/User.java
+++ src/cw/User.java
@@ -5,19 +5,19 @@ package cw;
 
 // 사용자 객체 클래스
 public class User {
-	private String id;	// 사용자 객체의 아이디를 저장
-	private String pwd;	// 사용자 객체의 비밀번호를 저장
+	private String id;	
+	private String pwd;	
 	
-	public String getId() {	// 사용자 객체의 id 반환
+	public String getId() {	
 		return id;
 	}
-	public void setId(String id) {	// 사용자 객체의 id 초기화
+	public void setId(String id) {	
 		this.id = id;
 	}
-	public String getPwd() {	// 사용자 객체의 pwd 반환
+	public String getPwd() {	
 		return pwd;
 	}
-	public void setPwd(String pwd) {	// 사용자 객체의 pwd 초기화
+	public void setPwd(String pwd) {	
 		this.pwd = pwd;
 	}
 }
